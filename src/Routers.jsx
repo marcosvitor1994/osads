b5dd0b0bc4933a379ddcaf39baa3cd73c6dc762b
Marcos Vitor <marcosvitor1994@gmail.com>
2021-12-01 17:35:23 -0300

    Atualização 17:35

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -4,17 +4,18 @@ import { Container } from 'react-bootstrap'
 import Inicio from './pages/Home'
 import Partituras from './pages/Partituras'
 import Maestro from './pages/Maestro'
+import Contato from './pages/Contato'
 
 
 const Routers = () => {
     return (
         <>
                 <Container className='md-3'>
-                    <Routes >
+                    <Routes>
                         <Route path="/home" element={<Inicio />} />
                         <Route path="/maestro" element={<Maestro />} />
                         <Route path="/partituras" element={<Partituras />} />             
-             
+                        <Route path="/contato" element={<Contato />} /> 
                     </Routes>
                 </Container>            
         </>
---
+++ src/pages/Contato.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contato = () => {
+    return (
+        <>
+            <p>Contato</p>
+        </>
+    )
+}
+
+export default Contato
