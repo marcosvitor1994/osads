de477930d4676cfceb1e16c41f97f08046fcc62c
Marcos Vitor Dias dos Santos <marcos.santos@mcom.gov.br>
2022-01-14 16:06:19 -0300

    Atualização dia 14/01/2022

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,81 +1,11 @@
-import React, {useState} from "react";
-import { Form, Button, Container, Row, Col, Card, FloatingLabel } from "react-bootstrap";
-import PropTypes from 'prop-types'
+import React from 'react'
 
 const Login = () => {
-
-
-  async function loginUser(credentials) {
-  return fetch('http://localhost:8080/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(credentials)
-  })
-    .then(data => data.json())
-  }
-
-  const [username, setUserName] = useState();
-  const [password, setPassword] = useState();
-  const [token, setToken] = useState([])
-  
-  const handleSubmit = async e => {
-    e.preventDefault();
-    const token = await loginUser({
-      username,
-      password
-    });
-    setToken(token);
-  }
-
-
   return (
-
-
     <>
-      <br />
-
-      <Container>
-        <Row>
-          <Col md={3}></Col>
-          <Col md={6}>
-            <Card className="bg-dark ">
-              <Card.Body>
-                <Card.Text>
-                  <>
-                    <FloatingLabel
-                      controlId="floatingInput"
-                      label="Endereço de Email"
-                      className="mb-3"
-                    >
-                      <Form.Control 
-                        onChange={e => setUserName(e.target.value)}
-                        type="email"
-                        placeholder="name@example.com"
-                      />
-                    </FloatingLabel>
-                    <FloatingLabel
-
-                      controlId="floatingPassword"
-                      label="Senha"
-                    >
-                      <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-                    </FloatingLabel>
-                  </>
-                  <br />
-                  <Button variant="primary" type="submit" onClick={handleSubmit}>
-                      Entrar
-                    </Button>
-                  
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      <p>Login</p>
     </>
-  );
-};
-Login.propTypes = { setToken: PropTypes.func.isRequired }
-export default Login;
+  )
+}
+
+export default Login
--- src/pages/Teste.jsx
+++ src/pages/Teste.jsx
@@ -1,87 +1,102 @@
-import React,{Component} from 'react'; 
-class App extends Component { 
+import axios from 'axios';
+import React from 'react';
+//import ReactDOM from 'react-dom';
 
-    state = { 
-      selectedFile: null
-    }; 
-     
-    onFileChange = event => { 
-      this.setState({ selectedFile: event.target.files[0] }); 
-    }; 
-     
-    onFileUpload = () => { 
+const appStyle = {
+    height: '250px',
+    display: 'flex'
+};
 
-      const f1 = new FormData(); 
-     
-      f1.append( 
-        "myFile", 
-        this.state.selectedFile,
-        this.state.selectedFile.name     
-      ); 
-      
-     
-      console.log(this.state.selectedFile);
-     
-      fetch(
-        "https://uploadFiles.marcosvitor6.repl.co/upload", 
-        {
-          method: 'POST',
-          body: f1,
-        }
-        ).then((response) => response.json()
-        ).then((result) => {
-          console.log('Success: ', result);
+const formStyle = {
+    margin: 'auto',
+    padding: '10px',
+    border: '1px solid #c9c9c9',
+    borderRadius: '5px',
+    background: '#f5f5f5',
+    width: '220px',
+    display: 'block'
+};
+
+const labelStyle = {
+    margin: '10px 0 5px 0',
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    fontSize: '15px',
+};
+
+const inputStyle = {
+    margin: '5px 0 10px 0',
+    padding: '5px', 
+    border: '1px solid #bfbfbf',
+    borderRadius: '3px',
+    boxSizing: 'border-box',
+    width: '100%'
+};
+
+const submitStyle = {
+    margin: '10px 0 0 0',
+    padding: '7px 10px',
+    border: '1px solid #efffff',
+    borderRadius: '3px',
+    background: '#3085d6',
+    width: '100%', 
+    fontSize: '15px',
+    color: 'white',
+    display: 'block'
+};
+
+const Field = React.forwardRef(({label, type}, ref) => {
+    return (
+      <div>
+        <label style={labelStyle} >{label}</label>
+        <input ref={ref} type={type} style={inputStyle} />
+      </div>
+    );
+});
+
+const Form = ({onSubmit}) => {
+    const usernameRef = React.useRef();
+    const passwordRef = React.useRef();
+    const handleSubmit = e => {
+        e.preventDefault();
+        const data = {
+            email: usernameRef.current.value,
+            senha: passwordRef.current.value
+        };
+        onSubmit(data);
+    };
+    return (
+      <form style={formStyle} onSubmit={handleSubmit} >
+        <Field ref={usernameRef} label="Email:" type="email" />
+        <Field ref={passwordRef} label="Senha:" type="password" />
+        <div>
+          <button style={submitStyle} type="submit">Submit</button>
+        </div>
+      </form>
+    );
+};
+
+// Usage example:
+
+const App = () => {
+    const handleSubmit = data => {
+        console.clear();
+        console.log(data)       
+        
+        axios.post('https://3000-indigo-platypus-sszf5uhk.ws-us27.gitpod.io/login', {data})
+        .then((result) => {
+          localStorage.setItem('token', result.data.token)
+          sessionStorage.setItem('token', result.data.token)
+          console.log('Resultado: ', result.data);
         }).catch((error) => {
-          console.error('Error: ', error);
+          console.log(error.response.data.message)
+          alert(error.response.data.message);
         })
+    };
+    return (
+      <div style={appStyle}>
+        <Form onSubmit={handleSubmit} />
+      </div>
+    );
+};
 
-    }; 
-     
-    // File content to be displayed after 
-    // file upload is complete 
-    fileData = () => { 
-      if (this.state.selectedFile) { 
-          
-        return ( 
-          <div> 
-            <h2>File Details:</h2> 
-            <p>File Name: {this.state.selectedFile.name}</p> 
-            <p>File Type: {this.state.selectedFile.type}</p> 
-            <p> 
-              Last Modified:{" "} 
-              {this.state.selectedFile.lastModifiedDate.toDateString()} 
-            </p> 
-          </div> 
-        ); 
-      } else { 
-        return ( 
-          <div> 
-            <br /> 
-            <h4>Choose before Pressing the Upload button</h4> 
-          </div> 
-        ); 
-      } 
-    }; 
-     
-    render() { 
-      return ( 
-        <div> 
-            <h1> 
-              GeeksforGeeks 
-            </h1> 
-            <h3> 
-              File Upload using React! 
-            </h3> 
-            <div> 
-                <input type="file" onChange={this.onFileChange}/> 
-                <button onClick={this.onFileUpload}> 
-                  Upload! 
-                </button> 
-            </div> 
-          {this.fileData()} 
-        </div> 
-      ); 
-    } 
-  } 
-  
-  export default App; 
+export default App;
