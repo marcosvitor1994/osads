416c9b68a03a933f1b7ddb57fab402d5605265d6
Marcos Vitor Dias dos Santos <marcos.santos@mcom.gov.br>
2022-01-24 16:43:10 -0300

    Atualização 24/01/2022 página login efetuada

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./login.css";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
-const Login = () => {
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const navigate = useNavigate();
+
+  function validateForm() {
+    return email.length > 0 && senha.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const data = {
+      email: email,
+      senha: senha
+    }
+    login(data)
+  }
+
+  const login = data => {
+
+    console.clear();
+    console.log(data);  
+
+    axios.post('https://3000-indigo-platypus-sszf5uhk.ws-us28.gitpod.io/login', {data})
+        .then((result) => {
+          
+          localStorage.setItem('token', result.data.token)
+          sessionStorage.setItem('token', result.data.token)
+          localStorage.setItem('_role', result.data.user._role)
+          sessionStorage.setItem('_role', result.data.user._role)
+          localStorage.setItem('email', result.data.user.email)
+          sessionStorage.setItem('email', result.data.user.email)
+          console.log('Resultado: ', result.data);
+
+          navigate('/partituras')
+
+        }).catch((error) => {
+          
+          alert(error.response.data.message);
+        })
+  }
+
   return (
-    <>
-      <p>Login</p>
-    </>
-  )
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control autoFocus type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Senha:</Form.Label>
+          <Form.Control type="password" value={senha} onChange={(e) => setSenha(e.target.value)} />
+        </Form.Group>
+        <br />
+        <Button block type="submit" disabled={!validateForm()}>
+          Entrar
+        </Button>
+      </Form>
+    </div>
+  );
 }
-
-export default Login
