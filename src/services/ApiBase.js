87df49124279b495314419054a0dcd9304f4ab27
Marcos Vitor <70297708+marcosvitor1994@users.noreply.github.com>
2022-04-01 08:44:36 -0300

    Update ApiBase.js
    Bug heroku ajustado.

--- src/services/ApiBase.js
+++ src/services/ApiBase.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const ApiBase = axios.create({
-    baseURL: 'https://4000-indigo-platypus-sszf5uhk.ws-us38.gitpod.io'
+    baseURL: 'https://orquestra-api.herokuapp.com'
 })
 
 export default ApiBase;
